97da53400fc4c65301b02bb6f61489a0d19be29e
Sudhir Kumar <sudhir210987@gmail.com>
2012-12-01 20:30:56 +0530

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  def gravatar_for(user)
+    #gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
+    #gravatar_url = "https://secure.gravatar.com/avatars/#{gravatar_id}.png"
+    gravatar_url = "http://www.gravatar.com/avatar/56ebf5d9540a728f7a535d3c250b540e.png"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Sudhir Kumar"
+    email "sudhir210987@gmail.com"
+    password "password"
+    password_confirmation "password"
+  end
+end
